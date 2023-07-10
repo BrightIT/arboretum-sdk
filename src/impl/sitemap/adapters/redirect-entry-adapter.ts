c61aa22d227b2758665c63a509a6c58620023b03
Dacjan Naumowicz <dacjan.naumowicz@bright-it.com>
2023-07-10 12:23:53 +0200

    Fix redirects paths

--- src/impl/sitemap/adapters/redirect-entry-adapter.ts
+++ src/impl/sitemap/adapters/redirect-entry-adapter.ts
@@ -1,45 +1,45 @@
-import { RedirectT } from '../../arboretum-client.impl';
+import { RedirectT } from "../../arboretum-client.impl";
 import {
   ContentTypeT,
   EntryT,
   LocaleT,
-} from '../../../clients/contentful-client/contentful-client';
-import { SitemapDataT } from '../../data/sitemap-data';
-import { localizeField } from '../helpers/localize-contentful-field';
+} from "../../../clients/contentful-client/contentful-client";
+import { SitemapDataT } from "../../data/sitemap-data";
+import { localizeField } from "../helpers/localize-contentful-field";
 
 export const redirectEntryAdapter = (
-  data: Pick<SitemapDataT, 'locales' | 'defaultLocaleCode'>,
-  pageField: ContentTypeT['fields'][number],
-  pathField: ContentTypeT['fields'][number],
-  typeField: ContentTypeT['fields'][number],
-  titleField: ContentTypeT['fields'][number] | undefined,
-  parent: RedirectT['parent'],
+  data: Pick<SitemapDataT, "locales" | "defaultLocaleCode">,
+  pageField: ContentTypeT["fields"][number],
+  pathField: ContentTypeT["fields"][number],
+  typeField: ContentTypeT["fields"][number],
+  titleField: ContentTypeT["fields"][number] | undefined,
+  parent: RedirectT["parent"],
   locale: LocaleT,
-  entry: EntryT,
+  entry: EntryT
 ): RedirectT | undefined => {
   const fieldValue = localizeField(data, locale);
 
   const page = fieldValue<{ sys?: { id?: string } }>(
     pageField.localized,
-    entry.fields[pageField.id],
+    entry.fields[pageField.id]
   );
   const path = fieldValue<string>(
     pathField.localized,
-    entry.fields[pathField.id],
+    entry.fields[pathField.id]
   );
   const type = fieldValue<string>(
     typeField.localized,
-    entry.fields[typeField.id],
+    entry.fields[typeField.id]
   );
   const title = titleField
     ? fieldValue<string>(titleField.localized, entry.fields[titleField.id])
     : undefined;
   const pageSysId = page?.sys?.id;
 
-  return pageSysId && path && (type === 'redirect' || type === 'alias')
+  return pageSysId && path && (type === "redirect" || type === "alias")
     ? {
         page: { sys: { id: pageSysId } },
-        path,
+        path: "/" + locale.code + path,
         title: title || undefined,
         metadata: entry.metadata,
         type,
